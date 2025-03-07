e363be0ad973bef988b146344bcf41af98bcd3ac
Himanshu Singh Rajput <158039836+Himanshu0721@users.noreply.github.com>
2025-03-07 09:48:58 +0530

    Update menuData.js

--- src/components/menuData.js
+++ src/components/menuData.js
@@ -4,7 +4,7 @@ const menuItems = [
     // { name: 'TEN Internships', url: 'https://internships.entrepreneurshipnetwork.net/' },
     // { name: 'TEN-Consulting', url: 'https://hrconsulting.entrepreneurshipnetwork.net/' },
     // { name: 'TEN HR Mentors', url: 'https://hr-mentor-nine.vercel.app/' },
-    { name: 'TEN Incubation', url: '#' },
+    { name: 'TEN Incubation', url: 'https://vc-incubation.vercel.app' },
     { name: 'TEN Funding', url: 'https://ten-funding-lime.vercel.app/' },
     
   ];
